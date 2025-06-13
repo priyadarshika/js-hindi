609b2c8c19a21abaef68f37ee1f0033ca81a85a9
priyadarshika <86980587+priyadarshika@users.noreply.github.com>
2025-06-13 16:38:02 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "priya@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2   not allowed 
+accountEmail= "ahgsbd@gmail.com"
+accountPassword = "1988675"
+accountCity = "bengluru"
+let accountState;
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of the use in block scope and functional scope
+
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")
+// alert( 3+3) we are using node JS not browser
+
+console.log(3+3); 
+
+console.log("darshi"); // code readablity that should be high
+
+// documentation on tc39.es and mdn 
+
+
+let name = "hitish"
+let age = 18
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined => 
+// symbol=>unique
+
+
+
+// object
+
+console.log(typeof null); // type = object
+console.log(typeof undefined);// type = undefine
+
+
