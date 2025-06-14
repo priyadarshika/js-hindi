893848087b1cab53062623da81bbb55018c1c16a
PRIYADARSHIKA <86980587+priyadarshika@users.noreply.github.com>
2025-06-14 17:40:45 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = true
+
+// console.log(typeof score); // console.log(typeof (score)) both are same
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc => NaN" true =>1; false=>0
+
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+
+// console.log( booleanIsLoggedIn);
+// 1 => true  0=> false
+// " " => false
+// "darshi" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+ 
+
+
+// node 03_conversionOperation.js
