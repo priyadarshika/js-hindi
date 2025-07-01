52033ee7630c958df0677a98ccba9589fcc2a18a
PRIYADARSHIKA <86980587+priyadarshika@users.noreply.github.com>
2025-07-01 16:24:34 +0000

    functions

---
+++ .devcontainer/02_basics/06_functions.js
@@ -0,0 +1,22 @@
+function calculateCartPrice(val1,val2,...num1){
+    return  num1
+}
+
+console.log(calculateCartPrice(200, 400, 500,2000));
+
+const user = {
+    username: "darshi",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`usernam is ${anyobject.username} and price is${anyobject.price})`);
+
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
